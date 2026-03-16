93742c1bac273e553374391c8e4eef57cf18789f
Teodoro Machado Farina <teodoro.mf17@gmail.com>
2026-03-16 16:22:29 -0300

    first commit

---
+++ teste_twig.php
@@ -0,0 +1,16 @@
+<?php
+require('carregar_twig.php');
+
+$nome = 'Théo Método';
+$disciplinas = [
+    'Programação',
+    'Banco de Dados',
+    'Interface Web',
+    'Desenvolvimento de Sistemas',
+];
+
+echo $twig->render('teste_twig.html', [
+    'nome' => $nome,
+    'legal' => true,
+    'disciplinas' => $disciplinas,
+]);
